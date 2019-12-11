1e8a734a3006dec2072567efe4fb3faad5b20145
xiaochuan1719 <xiaochuan1719@outlook.com>
2019-12-11 13:39:57 +0800

    ES6+代码的编译

--- 01-basic-examples/chapter02/dist/index.js
+++ 01-basic-examples/chapter02/dist/index.js
@@ -75,7 +75,7 @@
 
   var arr1 = [1, 2, 3];
   var arr2 = [4, 5, 6];
-  console.log([].concat(arr1, [arr2]));
+  console.log([].concat(arr1, arr2));
 
   function initDemo() {
     var data;
--- 01-basic-examples/chapter02/src/index.js
+++ 01-basic-examples/chapter02/src/index.js
@@ -3,7 +3,7 @@ import demo from './lib/demo';
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 
-console.log([...arr1, arr2]);
+console.log([...arr1, ...arr2]);
 
 async function initDemo () {
     let data = await demo();
